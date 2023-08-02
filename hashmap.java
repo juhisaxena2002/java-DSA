7d42313cec1b323ee8bf4a7ba53854164a14aeb7
juhisaxena2002 <126887332+juhisaxena2002@users.noreply.github.com>
2023-08-02 22:46:14 +0530

    Update hashmap.java

--- hashmap.java
+++ hashmap.java
@@ -5,18 +5,18 @@ import java.util.Set;
 public class hashmap {
     public static void main(String[] args) {
         HashMap<Integer,String> hm = new HashMap<>(); 
-        hm.put(1,"Ritika");
-        hm.put(2, "Ritziii");
-        hm.put(3,"Ritzzz");
-        hm.put(4,"Riti" );
+        hm.put(1,"abc");
+        hm.put(2, "def");
+        hm.put(3,"ghi");
+        hm.put(4,"jkl" );
 
        System.out.println( hm.containsKey(3));
-       System.out.println(hm.containsValue("Ritika"));
+       System.out.println(hm.containsValue("abc"));
        System.out.println(hm.get(3));
        System.out.println(hm.get(5));
        hm.remove(2);
-       hm.put(5,"Juhi");
-      System.out.println(hm.getOrDefault(8, "jules"));
+       hm.put(5,"xyz");
+      System.out.println(hm.getOrDefault(8, "rit"));
 
         System.out.println(hm);
         Set s = hm.entrySet();
